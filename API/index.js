13e4fa188ceb65533f18370de645de15664ec48b
edinjson <figueroaedinson@gmail.com>
2021-02-08 14:00:44 -0500

    Finalizado el backend, inicio frontend

--- API/index.js
+++ API/index.js
@@ -1,9 +1,20 @@
 //importar express
 const express = require('express');
+//conectar base de datos
+const mongoose = require('./database.js');
+//rutas
+const routes = require('./routes');
+const bodyParser = require('body-parser');
 
 //crear el servidor
 const app = express();
 
+//habilitar el body-parser // middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+//habilitar rutas
+app.use('/', routes());
 
 //puerto y arranque
 app.listen(4000, () =>{
---
+++ frontend/src/App.js
@@ -0,0 +1,9 @@
+
+
+function App() {
+  return (
+    <h1>Citas Veterinaria</h1>
+  );
+}
+
+export default App;
